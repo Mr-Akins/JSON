3d64460ef76e81e00e287da7413db921998002b8
Michael Akins <mrakins1216@ou.edu>
2019-02-25 11:48:05 -0600

    Class is over.

---
+++ JSON Participations/JSON Participations/HerokuQuote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_Participations
+{
+    class HerokuQuote
+    {
+        public string character { get; set; }
+
+        public string quote { get; set; }
+
+
+
+    }
+}
